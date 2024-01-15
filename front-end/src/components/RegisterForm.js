b9e30eb5bee12d5e0e4eeb473d0c5af7549613c8
Obando <damiobando@gmail.com>
2024-01-15 13:09:52 -0600

    npm install

--- front-end/src/components/RegisterForm.js
+++ front-end/src/components/RegisterForm.js
@@ -8,20 +8,23 @@ import Checkbox from '@mui/material/Checkbox';
 import { Button } from './Button';
 import { Link } from 'react-router-dom';
 
-
-
 function RegisterForm() {
+  const [firstName, setFirstName] = useState('');
+  const [secondName, setSecondName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+  const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
-    const [showPassword, setShowPassword] = useState(false);
-  
-    const handleClickShowPassword = () => setShowPassword((show) => !show);
-  
-    const handleMouseDownPassword = (event) => {
-      event.preventDefault();
-    };
-    const [click, setClick] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
@@ -41,60 +44,77 @@ function RegisterForm() {
 
   window.addEventListener('resize', showButton);
 
-    return (
-        <>
-    <div className='signUp_container'>      
-    
-    <div className='register_container '>
-    
-    <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 2, width: '45ch' },
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'left',
-        alignContent: 'left',
-        marginLeft: '-200px'
-      }}
-      noValidate
-      autoComplete="off" >
+  const handleButtonClick = () => {
+    console.log('Primer Nombre:', firstName);
+    console.log('Segundo Nombre:', secondName);
+    console.log('Correo Electrónico:', email);
+    console.log('Contraseña:', password);
+  };
 
-        <h1>Sign In to your Account</h1>  
-        <TextField
-          required
-          id="firstName"
-          label="Primer Nombre"
-        />
-         <TextField
-          required
-          id="secondName"
-          label="Segundo Nombre"
-        />
-         <TextField
-          required
-          id="email"
-          label="Correo Electrónico"
-        />
-        <TextField
-          required
-          id="password"
-          label="Contraseña"
-          type="password"
-        />
-        </Box>
+  return (
+    <>
+      <div className='signUp_container'>
+        <div className='register_container '>
+          <Box
+            component='form'
+            sx={{
+              '& .MuiTextField-root': { m: 2, width: '45ch' },
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'left',
+              alignContent: 'left',
+              marginLeft: '-200px',
+            }}
+            noValidate
+            autoComplete='off'
+          >
+            <h1>Sign In to your Account</h1>
+            <TextField
+              required
+              id='firstName'
+              label='Nombre'
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <TextField
+              required
+              id='secondName'
+              label='Apellidos'
+              value={secondName}
+              onChange={(e) => setSecondName(e.target.value)}
+            />
+            <TextField
+              required
+              id='email'
+              label='Correo Electrónico'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              required
+              id='password'
+              label='Contraseña'
+              type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Box>
         </div>
-        <div className='logoBtn' >
-        <img src='./images/LogoIntercambio.png' style={{ width: '400px', height: 'auto', alignContent: 'center', marginLeft : '100px' }}/>
-        <FormGroup>
-        <FormControlLabel required control={<Checkbox />} label="Acepta los Términos y Condiciones" />
-        </FormGroup>
-        <Button buttonStyle='btn--outline'>Crear cuenta</Button>
- 
+        <div className='logoBtn'>
+          <img
+            src='./images/LogoIntercambio.png'
+            style={{ width: '400px', height: 'auto', alignContent: 'center', marginLeft: '100px' }}
+          />
+          <FormGroup>
+            <FormControlLabel required control={<Checkbox />} label='Acepta los Términos y Condiciones' />
+          </FormGroup>
+          <Button buttonStyle='btn--outline' onClick={handleButtonClick}>
+            Crear cuenta
+          </Button>
         </div>
-    </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
+}
 
-export default RegisterForm
+export default RegisterForm;
